b270c7a52e5dac8725b8fc67ec3dd326ca88f663
sutt <wsutton17@gmail.com>
2016-11-25 12:28:25 -0500

    added some boilerplate to runtest to remeber as a template

--- hinomaru/runtest.py
+++ hinomaru/runtest.py
@@ -1,6 +1,8 @@
 from test.TestPreProc import ScratchTest
 import unittest
 from test.TestPreProc import main
+#from test.scratch2 import TestClass
+
 
 #suite = unittest.TestLoader().loadTestsFromTestCase(TestClass)
 #unittest.TextTestRunner(verbosity=2).run(suite)
