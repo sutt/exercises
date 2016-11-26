98784ea8b4fc1e338b47981c2a4f14f8cd330a20
sutt <wsutton17@gmail.com>
2016-11-26 16:44:18 -0500

    refactoring for centralized logging and removing Board

--- connectfour/cf.py
+++ connectfour/cf.py
@@ -1,44 +1,42 @@
-import os, sys, random
+import os, sys, random, copy, time
 
 from src.utils import Board
 from src.play import Play
+from src.utils import Log
 
 BOARD_WIDTH = 8
 BOARD_HEIGHT = 8
-#INIT_STATE = [[0 for row in range(BOARD_WIDTH)] for col in range(BOARD_HEIGHT)]  
-import copy
-
-# class Data:
-#     INIT_STATE = [[0 for row in range(BOARD_WIDTH)] for col in range(BOARD_HEIGHT)]
+INIT_STATE = [[0 for row in range(BOARD_WIDTH)] for col in range(BOARD_HEIGHT)]
 
 def main():
     
-    board = Board(BOARD_WIDTH,BOARD_HEIGHT)
-    INIT_STATE = [[0 for row in range(BOARD_WIDTH)] for col in range(BOARD_HEIGHT)]    
+    board = Board(BOARD_WIDTH,BOARD_HEIGHT)  #refactor out board
+    log = Log(noisy = True)
 
-    for game in range(10):
-        print "GAME: ", str(game)
+    for game_i in range(10):
         
-        local_data = copy.deepcopy(INIT_STATE)
-        play = Play(board = board, state = local_data)
+        log.game_start(game_i = game_i)       
+        play = Play(board = board, state = copy.deepcopy(INIT_STATE))
         
-        for i in range(100):
+        for i in range(BOARD_WIDTH*BOARD_HEIGHT + 1):
             
+            #ACTION-SPACE
             ap = play.available_plays()
-
             if len(ap) < 1:
-                print 'Game Ends in Draw at i: ', str(i)
+                log.game_draw()
                 break
-
+            
+            #DECISION-ACTION
             play.make_play(random.sample(ap,1)[0])
 
-            if play.check_win(play = i):
-                if play.extra:  #a diag-win
-                    print 'WINNING at i:', str(i)
-                    board.print_board(board_state = play.state[:], board_header = True)
+            #OBSERVE
+            if play.check_win():    #this writes to log too
+                log.game_win(win_state = play.state)    
                 break
 
-    
+        log.game_end()
+
+    log.end_runs()
 
 
 if __name__ == "__main__":
--- connectfour/src/utils.py
+++ connectfour/src/utils.py
@@ -1,10 +1,107 @@
+import time,os,sys,random,copy
 
+game = {}
+game['game_i'] = None
+game['t0'] = None
+game['t1'] = None
+game['t'] = None
+game['count_turn'] = None
+game['win_bool'] = None
+game['win_turns'] = None
+game['win_type'] = None
+game['player_win'] = None
+
+
+class Log:
+    
+    
+
+
+    def __init__(self, noisy = False, **kwargs):
+        
+        self.game = copy.copy(game)
+
+        self.games = []
+
+        self.noisy = noisy
+        self.record = False
+        self.persist = False
+
+    
+    def reset_game(self):
+        pass
+
+    def game_start(self, game_i = None, noisy = False ):
+        
+        self.game = copy.copy(game)    #reset
+        
+        self.game['count_turn'] = 0
+
+        self.game['t0'] =  time.time()
+        
+        self.game['game_i'] = game_i 
+        
+        if noisy or self.noisy:
+            print "GAME: ", str(game_i)
+
+
+    def print_board(self, state):
+    
+        _blank, _X, _O = "~", "X", "O"
+        chars = [_blank,_X,_O]
+
+        lines = []
+        for irow in range(len(state)-1,-1,-1):
+            line = [chars[spot] for spot in state[irow]]  
+            lines.append(line)
+        
+        out = ""
+        for line in lines:
+            out += " ".join(line) + "\n"
+        print out
+
+
+    def game_draw(self, noisy = False):
+        
+        self.game['win_bool']
+        if noisy or self.noisy:
+            print 'Game Ends in Draw at i: ', str(self.game_i)
+
+    def game_win(self, win_state = None, noisy = False):
+
+        if noisy or self.noisy:
+            print 'WINNING at i:', str(self.game.get('win_turns', 'TURN_UNKNOWN'))
+            if win_state is not None:
+                self.print_board(win_state)
+
+    def game_end(self,noisy = False):
+        
+        self.game['t1'] = time.time()
+        
+        if self.game['t0'] is not None:
+            self.game['t'] = self.game['t1'] - self.game['t0']
+
+        if self.record:
+            self.games.append(game)
+
+        if noisy or self.noisy:
+            pass        
+            
+    def end_runs(self):
+        print 'ENDING -----'
+        if self.persist:
+            #writes games to file as json
+            pass
+
+
+    
 class Board:
-
+    
     def __init__(self,width,height):
         self.width = width
         self.height = height
-
+    
+    #""" Deprecated stuff: -------------------------"""
     def print_board(self,**kwargs):
         
         _blank, _X, _O = "~", "X", "O"
@@ -12,11 +109,11 @@ class Board:
 
         lines = [ list(_blank * self.width) for _ in range(self.height)]
         
-        state = kwargs.get('board_state',[])
+        state = kwargs.get('state',[])
         if len(state) != 0:
             lines = []
-            for row in range(len(state)-1,-1,-1):
-                line = [chars[spot] for spot in state[row]]  
+            for irow in range(len(state)-1,-1,-1):
+                line = [chars[spot] for spot in state[irow]]  
                 lines.append(line)
         
         out = ""
