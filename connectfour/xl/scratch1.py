d83ce7e85fd682637ba864ef9e5a02fa0c2c5609
sutt <wsutton17@gmail.com>
2017-01-04 16:50:58 -0500

    more stuff for xl reporting

--- connectfour/xl/scratch1.py
+++ connectfour/xl/scratch1.py
@@ -2,6 +2,8 @@ import xlwings as xw
 import json
 import yaml
 import sys, os
+import argparse
+
 
 game = {}
 game['game_i'] = None
@@ -13,6 +15,13 @@ game['win_bool'] = None
 game['win_turns'] = None
 game['win_type'] = None
 game['win_player'] = None
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("--excel", action="store_true", default=False)
+ap.add_argument("--csv", action="store_true", default=False)
+args = vars(ap.parse_args())
+
 
 def load_file(path):
     
@@ -48,16 +57,76 @@ def all_files(low,high,**kwargs):
         
 
 def xl():
-    wb = xw.Book()  # this will create a new workbook
-    #wb = xw.Book('FileName.xlsx')  # connect to an existing file in the current working directory
-    #wb = xw.Book(r'C:\users\william\desktop\')  # on Windows: use raw strings to escape backslashes
-    sht = wb.sheets['Sheet1']
+    #wb = xw.Book()  # this will create a new workbook
+    wb = xw.Book('scratch1.xlsx')  # connect to an existing file in the current working directory
+    sht = wb.sheets['Sheet2']
     return sht
+
+
+def new_output(all_files):
+    
+    i = 1
+    f_prefix = "output"
+    while True:
+        fn = f_prefix + str(i) + ".csv"
+        if fn in all_files:
+            i += 1
+        else:
+            return fn
+
+
+def output_name(**kwargs):
+    
+    datadir = os.path.join( os.getcwd(), 'data')
+    all_files = os.listdir(datadir)
+    fn = new_output(all_files)
+    fnpath = os.path.join( os.getcwd(), 'data', fn )
+    return fnpath
+
+
+def make_csv(input_data):
+    """ input_data: nested list 
+        returns : list of stings, collapsed into csv """
+    s_data = [map(str, row) for row in input_data]
+    s2_data = [ reduce(lambda x,y: x + "," + str(y), row) for row in s_data]
+    return "\n".join(s2_data)
     
 
-rows = all_files(95,96)
-sht = xl()
-down,across = len(rows), len(rows[0])
-for i in range(down):
-    for j in range(across):
-        sht[i,j].value = rows[i][j] 
+
+
+def main():
+
+    rows = all_files(9,9)
+
+    down, across = len(rows), len(rows[0])
+
+    if args["csv"]:
+        
+        out_data = make_csv(rows)
+        fnpath = output_name()
+        print 'outputting to: ' , fnpath
+
+        f = open(fnpath,'w')
+        f.writelines(out_data)
+        f.close()
+
+    if args["excel"]:
+        
+        sht = xl()
+        for i in range(down):
+            for j in range(across):
+                sht[i,j].value = rows[i][j] 
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
