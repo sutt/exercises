204007638b4579fd67328fe1d4423a79bc204c53
sutt <wsutton17@gmail.com>
2017-01-09 16:50:50 -0500

    adding soduku solver

---
+++ sudoku/quick.py
@@ -0,0 +1,58 @@
+import os,sys, copy
+
+p1 = """
+410070005
+080006090
+000500000
+007401300
+530000012
+004308700
+000004000
+090800070
+700060028
+"""
+
+def text_to_puzzle(text):
+    return filter(str.isdigit,text)
+    
+def string_to_puzzle(string, spaces = False):
+    s = ""
+    for i in range(9):
+       line = string[ (i*9) : ((i+1)*9)]
+       if spaces: 
+           line = ' '.join(list(line))
+       s += line
+       s+= "\n"
+    return s
+
+def ij_to_c(i,j): return (i * 8) + j
+
+def c_to_ij(c): return ((c/9),c%9)
+
+def return_shares():
+    board = range(81)
+    cols = [[c for c in board if (c%9) == col ] for col in range(9)]
+    rows = [[c for c in board if (c/9) == row ] for row in range(9)]
+    areas = [[c for c in board if c_to_ij(c)[0] / 3 == i and c_to_ij(c)[1] / 3 == j ] for i in range(3) for j in range(3)]   #across, then down
+    return rows, cols, areas
+
+rows, cols, areas = return_shares()
+
+
+
+#puzzle81 = text_to_puzzle(p1)
+#print string_to_puzzle(puzzle81, spaces=True)
+
+
+
+
+
+#working stuff --------
+if False:
+    puzzle81 = text_to_puzzle(p1)
+    print puzzle81
+    print 'len p1:', str(len(p1))
+    print 'len puzzle81:', str(len(puzzle81))
+
+    for i in range(12):
+        print str(i), " : ", str(puzzle81[i])
